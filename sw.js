b9fb3f4b8ff6848d0007a0459eff119f10852090
estudionube82-cyber <estudionube82@gmail.com>
2026-04-13 14:03:32 -0300

    Finalize SW hotfix with fully neutral self-unregistering worker

--- sw.js
+++ sw.js
@@ -1,29 +1,10 @@
-// HOTFIX de emergencia:
-// Este Service Worker se auto-desactiva para estabilizar la SPA.
+// Service Worker desactivado intencionalmente.
+// No registra caché, no fuerza activación y se desregistra solo.
 
 self.addEventListener('install', () => {
-  self.skipWaiting();
+  console.log('[SW] deshabilitado');
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    try {
-      const cacheNames = await caches.keys();
-      await Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
-    } catch (err) {
-      console.warn('[SW] Error limpiando cachés:', err);
-    }
-
-    await self.registration.unregister();
-    await self.clients.claim();
-
-    const clients = await self.clients.matchAll({
-      type: 'window',
-      includeUncontrolled: true,
-    });
-
-    for (const client of clients) {
-      client.navigate(client.url);
-    }
-  })());
+  event.waitUntil(self.registration.unregister());
 });
